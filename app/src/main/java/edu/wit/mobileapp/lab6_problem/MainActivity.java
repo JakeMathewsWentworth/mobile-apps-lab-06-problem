108288bb14ca71b14866f7a17a49c69a659a8eb9
Jake Mathews <mathewsj2@wit.edu>
2018-08-15 14:24:15 -0400

    Upload

---
+++ app/src/main/java/edu/wit/mobileapp/lab6_problem/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.wit.mobileapp.lab6_problem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "myApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        for(int i = 0; i<50; i++){
+            adapter.add("item_"+i);
+        }
+
+        ListView listView = (ListView)findViewById(R.id.ListView01);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+                Log.v(TAG, "item " + position + " is clicked");
+            }
+        });
+    }
+}
